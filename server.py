9370b94dc8447a4a3a78ea5b36487bd4ca8cb3e4
Allen Czerwinski <allen.czerwinski@gmail.com>
2015-11-07 13:40:21 -0800

    require fields not be blank and require first and last name only contain letters

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, session, flash
+import re
 app = Flask(__name__)
 app.secret_key="SecretKeyMePls"
 @app.route('/')
@@ -12,5 +13,12 @@ def submit():
 		session['last_name'] = request.form['last_name']
 		session['password'] = request.form['password']
 		session['confirm_password'] = request.form['confirm_password']
+		# Validates that all fields are required and must not be blank
+		if len(session['email']) < 1 or len(session['first_name']) < 1 or len(session['last_name']) < 1 or len(session['password']) < 1 or len(session['confirm_password']) < 1:
+			flash("No fields can be empty!")
+			return redirect('/')
+		# Validates that First and Last Name cannot contain any numbers
+		elif not session['first_name'].isalpha() or not session['last_name'].isalpha():
+			flash("First and Last name must contain only letters!")
 		return redirect('/')
 app.run(debug=True)
