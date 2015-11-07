165ccb13ab28ecc9e897b55353d178bf6eb581df
Allen Czerwinski <allen.czerwinski@gmail.com>
2015-11-07 15:33:36 -0800

    add birthdate and validation that birthdate is correct format and in the past

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template, request, redirect, session, flash
 import re
+import time
 EMAIL_REGEX = re.compile(r'^[a-za-z0-9\.\+_-]+@[a-za-z0-9\._-]+\.[a-za-z]*$')
 PASSWORD_CHECKER = re.compile(r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$')
+BIRTHDATE_CHECKER = re.compile(r'(\d{4}/\d{2}/\d{2})')
+todays_date = "2015/11/07"
+compare_date = time.strptime(todays_date, "%Y/%m/%d")
 app = Flask(__name__)
 app.secret_key="SecretKeyMePls"
 @app.route('/')
@@ -13,10 +17,12 @@ def submit():
 		session['email'] = request.form['email']
 		session['first_name'] = request.form['first_name']
 		session['last_name'] = request.form['last_name']
+		session['birth_date'] = request.form['birth_date']
+		user_date = time.strptime(session['birth_date'], "%Y/%m/%d")
 		session['password'] = request.form['password']
 		session['confirm_password'] = request.form['confirm_password']
 		# Verifies that all fields are required and must not be blank
-		if len(session['email']) < 1 or len(session['first_name']) < 1 or len(session['last_name']) < 1 or len(session['password']) < 1 or len(session['confirm_password']) < 1:
+		if len(session['email']) < 1 or len(session['first_name']) < 1 or len(session['last_name']) < 1 or len(session['birth_date']) < 1 or len(session['password']) < 1 or len(session['confirm_password']) < 1:
 			flash("No fields can be empty!")
 			return redirect('/')
 		# Verifies that First and Last Name cannot contain any numbers
@@ -38,8 +44,14 @@ def submit():
 		elif not PASSWORD_CHECKER.match(session['password']):
 			flash("Password must contain at least one uppercase letter and 1 number!")
 			return redirect('/')
+		elif not BIRTHDATE_CHECKER.match(session['birth_date']):
+			flash("Birthdate appears to be in the wrong format. Make sure you're using 'yyyy/mm/dd' please!")
+			return redirect('/')
+		elif user_date > compare_date:
+			flash("Birthdate has to be in the past!")
+			return redirect('/')
 		# Thanks user for submitting their information	
 		else:
-			flash("Thanks for submitting your information.")
+			flash("Thanks for submitting your information!")
 			return redirect('/')
 app.run(debug=True)
